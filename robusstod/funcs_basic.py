5b33a521ec53c8149b87a97d1d7cae248a600ce8
alexeynarykov <a.narykov@liverpool.ac.uk>
2023-07-28 16:59:10 +0100

    Moved get_noise_coefficients to funcs_basic.py and funcs_godot.py.

--- robusstod/funcs_basic.py
+++ robusstod/funcs_basic.py
@@ -2,6 +2,18 @@ import numpy as np
 import torch
 
 
+def get_noise_coefficients(GM):
+    """ A function that returns adequate noise coefficients for Van Loan's method, ideally from physical considerations.
+    We need these values that drive the process noise in the prediction step.
+    """
+    # ratio = 1e-2  # 2 orders are suggested by Simon following a Lee's manuscript
+    ratio = 1e-14  # otherwise it `lands in Bolivia'
+    st_dev = ratio * GM
+    q = st_dev ** 2
+    q_xdot, q_ydot, q_zdot = q, q, q
+    return np.array([q_xdot, q_ydot, q_zdot])
+
+
 def KeplerianToCartesian(K, GM, ndim, mapping_position, mapping_velocity):
     """ Based on KeplerianToCartesian from https://github.com/alecksphillips/SatelliteModel/blob/main/transforms.py """
 
--- robusstod/funcs_godot.py
+++ robusstod/funcs_godot.py
@@ -6,6 +6,18 @@ from godot.model import common
 from godot import cosmos
 
 
+def get_noise_coefficients(GM):
+    """ A function that returns adequate noise coefficients for Van Loan's method, ideally from physical considerations.
+    We need these values that drive the process noise in the prediction step.
+    """
+    # ratio = 1e-2  # 2 orders are suggested by Simon following a Lee's manuscript
+    ratio = 1e-14  # otherwise it `lands in Bolivia'
+    st_dev = ratio * GM
+    q = st_dev ** 2
+    q_xdot, q_ydot, q_zdot = q, q, q
+    return np.array([q_xdot, q_ydot, q_zdot])
+
+
 def KeplerianToCartesian(K, mu_earth, ndim, mapping_position, mapping_velocity):
     """ The function is based on https://godot.io.esa.int/tutorials/T04_Astro/T04scv/ """
 
