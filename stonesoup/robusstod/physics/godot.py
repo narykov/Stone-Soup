1e7c049d7cca4f23be4bb4835263909753bb5e0a
alexeynarykov <a.narykov@liverpool.ac.uk>
2023-08-07 14:14:43 +0100

    Updated godot.py to actually include necessary classes and not be a duplicate of basic.py.

--- stonesoup/robusstod/physics/godot.py
+++ stonesoup/robusstod/physics/godot.py
@@ -1,44 +1,51 @@
 import numpy as np
 import torch
+from godot.core import astro
+from godot.core import tempo, util, autodif as ad
+from godot.model import common
+from godot import cosmos
 
 
-def KeplerianToCartesian(K, GM, ndim, mapping_position, mapping_velocity):
-    """ Based on KeplerianToCartesian from https://github.com/alecksphillips/SatelliteModel/blob/main/transforms.py """
+def KeplerianToCartesian(K, mu_earth, ndim, mapping_position, mapping_velocity):
+    """ The function is based on https://godot.io.esa.int/tutorials/T04_Astro/T04scv/ """
 
-    a, e, i, w, omega, nu = K[:]  # elements i, w, omega, nu need to be in radians, a in metres
-
-    E = np.arctan2(np.sqrt(1 - e ** 2) * np.sin(nu), e + np.cos(nu))
-
-    x = a * (np.cos(E) - e)
-    y = a * np.sqrt(1 - e * e) * np.sin(E)
-
-    P = np.array([np.cos(omega) * np.cos(w) - np.sin(omega) * np.cos(i) * np.sin(w),
-                  np.sin(omega) * np.cos(w) + np.cos(omega) * np.cos(i) * np.sin(w), np.sin(i) * np.sin(w)])
-    Q = np.array([-np.cos(omega) * np.sin(w) - np.sin(omega) * np.cos(i) * np.cos(w),
-                  np.cos(omega) * np.cos(i) * np.cos(w) - np.sin(omega) * np.sin(w), np.sin(i) * np.cos(w)])
-
-    [X, Y, Z] = x * P + y * Q
-
-    r = a * (1 - e * np.cos(E))
-
-    x_dot = -((np.sqrt(a * GM)) * np.sin(E)) / float(r)
-    y_dot = ((np.sqrt(a * GM)) * np.sqrt(1 - e * e) * np.cos(E)) / float(r)
-
-    [U, V, W] = x_dot * P + y_dot * Q
+    xyz = astro.cartFromKep(K, mu_earth)
 
     state_vector = np.zeros(ndim)
-    state_vector[[mapping_position]] = [X, Y, Z]
-    state_vector[[mapping_velocity]] = [U, V, W]
+    state_vector[[mapping_position]] = xyz[:3]
+    state_vector[[mapping_velocity]] = xyz[3:]
 
     return state_vector
 
 
-def twoBody3d_da(state, GM=None):
-    if GM is None:
-        GM = 398600400000000.0
-
+def twoBody3d_da(state, **kwargs):
     (x, x_dot, y, y_dot, z, z_dot) = state
-    r_pow_3 = torch.float_power(torch.linalg.vector_norm(torch.tensor((x, y, z))), 3)
-    return (x_dot, -GM * x / r_pow_3,
-            y_dot, -GM * y / r_pow_3,
-            z_dot, -GM * z / r_pow_3)
+    uni = cosmos.Universe(cosmos.util.load_yaml("universe.yml"))
+    tensor1 = torch.tensor([x, y, z, x_dot, y_dot, z_dot], requires_grad=True)
+    pos = ad.Vector(tensor1.detach().numpy(), 'x0')  # x, y, z, xdot, ydot, zdot
+
+    tscale = tempo.TimeScale.TDB
+
+    epoch = tempo.XEpoch()
+    satellite = uni.frames.addPoint("Satellite", tscale)
+    icrf = uni.frames.axesId('ICRF')
+    earth = uni.frames.pointId('Earth')
+
+    translation_model = common.ConstantVectorTimeEvaluable(pos)
+    uni.frames.addTimeEvaluableTranslation(satellite, earth, icrf, translation_model)
+
+    dyn = uni.dynamics.get("Earth")
+
+    uni.frames.setAlias(dyn.point, satellite)
+    uni.frames.setAlias(dyn.coi, earth)
+    uni.frames.setAlias(dyn.axes, icrf)
+
+    try:
+        accel_godot = dyn.acc.eval(epoch)
+    except RuntimeError as e:
+        print(e)
+    accel = accel_godot.value() * ((1e3) ** 3)
+
+    return (x_dot, torch.tensor(accel[0], requires_grad=False),
+            y_dot, torch.tensor(accel[1], requires_grad=False),
+            z_dot, torch.tensor(accel[2], requires_grad=False))
