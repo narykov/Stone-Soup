aa3bdb13bc2d200a88b16a947e807e69c08f255f
alexeynarykov <a.narykov@liverpool.ac.uk>
2023-08-11 10:55:03 +0100

    Renamed the function for clarity

--- stonesoup/robusstod/physics/basic.py
+++ stonesoup/robusstod/physics/basic.py
@@ -33,7 +33,7 @@ def KeplerianToCartesian(K, GM, ndim, mapping_position, mapping_velocity):
     return state_vector
 
 
-def twoBody3d_da(state, GM=None):
+def diff_equation(state, GM=None):
     if GM is None:
         GM = 398600400000000.0
 
