a7d76e8830bfdd375e160af6c0433c6bc663a1ea
alexeynarykov <a.narykov@liverpool.ac.uk>
2023-09-27 15:14:58 +0100

    Added AugmentedGaussianStatePrediction fot the state SLR. Now it is working.

--- stonesoup/robusstod/stonesoup/types.py
+++ stonesoup/robusstod/stonesoup/types.py
@@ -1,11 +1,17 @@
 from ...base import Property
+from ...types.state import GaussianState
 from ...types.array import CovarianceMatrix
-from ...types.prediction import GaussianStatePrediction
+from ...types.prediction import Prediction
 
 
-class AugmentedGaussianStatePrediction(GaussianStatePrediction):
-    """ Prediction type
-
-    This is the base prediction class. """
+class AugmentedGaussianState(GaussianState):
+    """ This is a new GaussianState class that can also store information on cross-covariance
+    between the two uncertain kinematic states. We need it report augmented predictions which is otherwise not
+    possible."""
     cross_covar: CovarianceMatrix = Property(
         default=None, doc='Cross-covariance for the SLR algorithm')
+
+
+class AugmentedGaussianStatePrediction(Prediction, AugmentedGaussianState):
+    """ Prediction class for AugmentedGaussianState. The existence of this class harmonises how state predictions
+    are reported, to be in line with measurement predictions, which also carry information on cross-covariance. """
