275c45a004a3cc8eadf708038a6a243d9803ff13
alexeynarykov <a.narykov@liverpool.ac.uk>
2023-09-27 10:35:13 +0100

    Added AugmentedGaussianStatePrediction fot the state SLR.

--- stonesoup/robusstod/stonesoup/types.py
+++ stonesoup/robusstod/stonesoup/types.py
@@ -1,15 +1,11 @@
-from ...types.base import Type
 from ...base import Property
-from ...models.transition.base import TransitionModel
-from ...types.state import CreatableFromState
 from ...types.array import CovarianceMatrix
+from ...types.prediction import GaussianStatePrediction
 
 
-class Prediction(Type, CreatableFromState):
+class AugmentedGaussianStatePrediction(GaussianStatePrediction):
     """ Prediction type
 
     This is the base prediction class. """
-    transition_model: TransitionModel = Property(
-        default=None, doc='The transition model used to make the prediction')
     cross_covar: CovarianceMatrix = Property(
         default=None, doc='Cross-covariance for the SLR algorithm')
