790f6f5e432afccd8ed098d2cae712775a4b9afe
alexeynarykov <a.narykov@liverpool.ac.uk>
2023-10-04 14:41:54 +0100

    Current version.

--- robusstod/single_target_tracking_with_no_data_association.py
+++ robusstod/single_target_tracking_with_no_data_association.py
@@ -17,14 +17,14 @@ from stonesoup.types.array import CovarianceMatrix
 from stonesoup.types.hypothesis import SingleHypothesis
 from stonesoup.types.detection import TrueDetection
 from stonesoup.types.groundtruth import GroundTruthPath, GroundTruthState
-from stonesoup.types.prediction import GaussianState
+from stonesoup.types.prediction import GaussianState, GaussianStatePrediction
 from stonesoup.types.track import Track
 from stonesoup.updater.kalman import IteratedKalmanUpdater
 
 # ROBUSSTOD MODULES
 from stonesoup.robusstod.stonesoup.models.transition import LinearisedDiscretisation
 from stonesoup.robusstod.stonesoup.predictor import ExtendedKalmanPredictor
-from stonesoup.robusstod.stonesoup.smoother import IPLSKalmanSmoother, IPLSKalmanSmootherFull
+from stonesoup.robusstod.stonesoup.smoother import IPLSKalmanSmoother
 from stonesoup.robusstod.stonesoup.updater import IPLFKalmanUpdater
 from stonesoup.robusstod.physics.constants import G, M_earth
 from stonesoup.robusstod.physics.other import get_noise_coefficients
@@ -47,7 +47,7 @@ def do_single_target_tracking(prior=None, predictor=None, updater=None, measurem
     if measurements is None:
         measurements = []
 
-    track = Track()
+    track = Track([prior])
     for measurement in measurements:
         prediction = predictor.predict(prior, timestamp=measurement.timestamp)
         hypothesis = SingleHypothesis(prediction, measurement)  # Group a prediction and measurement
@@ -63,9 +63,9 @@ def main():
     start_time = datetime(2000, 1, 1)
     time_parameters = {
         'n_time_steps': 10,
-        'time_interval': timedelta(seconds=10)
+        'time_interval': timedelta(seconds=40)
     }
-    # TODO: consider arbitrary time intervals to demonstrate flexibility of the approach
+    # TODO: consider arbitrary time intervals to demonstrate the flexibility of the approach
     timesteps = [start_time + k * time_parameters['time_interval'] for k in range(time_parameters['n_time_steps'])]
 
     # We begin by specifying the true initial target state by picking a credible set of Keplerian elements, and
@@ -95,18 +95,25 @@ def main():
     GM = G * M_earth  # https://en.wikipedia.org/wiki/Standard_gravitational_parameter (m^3 s^−2)
     initial_state_vector = KeplerianToCartesian(K, GM, ndim_state, mapping_location, mapping_velocity)  # into Cartesian
     initial_state = GroundTruthState(state_vector=initial_state_vector, timestamp=start_time)
-    initial_covariance = CovarianceMatrix(np.diag([50000 ** 2, 100 ** 2, 50000 ** 2, 100 ** 2, 50000 ** 2, 100 ** 2]))
+    initial_covariance = CovarianceMatrix(np.diag([40000 ** 2, 100 ** 2, 40000 ** 2, 100 ** 2, 40000 ** 2, 100 ** 2]))
 
     deviation = np.linalg.cholesky(initial_covariance).T @ np.random.normal(size=initial_state.state_vector.shape)
     # deviation = np.array([-122068.01433784, 69.37315652, 507.76211348, -86.74038986, -58321.63970861, 89.04789997]).reshape((6, 1))
-    prior = GaussianState(state_vector=initial_state.state_vector + deviation,
-                          covar=initial_covariance,
-                          timestamp=start_time)
+    # prior = GaussianState(state_vector=initial_state.state_vector + deviation,
+    #                       covar=initial_covariance,
+    #                       timestamp=start_time)
+    prior = GaussianStatePrediction(state_vector=initial_state.state_vector + deviation,
+                                    covar=initial_covariance,
+                                    timestamp=start_time)
 
     transition_model = LinearisedDiscretisation(
         diff_equation=diff_equation,
         linear_noise_coeffs=get_noise_coefficients(GM)
     )
+    # from stonesoup.models.transition.linear import CombinedLinearGaussianTransitionModel, ConstantVelocity
+    # q = 1
+    # transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(q),
+    #                                                           ConstantVelocity(q), ConstantVelocity(q)])
 
     # Generate ground truth trajectory, using the initial target state, target dynamics and the grid of timesteps
     truth = GroundTruthPath(initial_state)
@@ -135,7 +142,7 @@ def main():
     )
 
     measurements = []
-    for state in truth:
+    for state in truth[1:]:
         measurement = measurement_model.function(state=state, noise=True)
         timestamp = state.timestamp
         measurements.append(TrueDetection(
@@ -153,9 +160,7 @@ def main():
     # Perform tracking/filtering/smooting
     track_iplf = do_single_target_tracking(prior=prior, predictor=predictor, updater=updater_iplf, measurements=measurements)
     track_iekf = do_single_target_tracking(prior=prior, predictor=predictor, updater=updater_iekf, measurements=measurements)
-    smoother = IPLSKalmanSmootherFull(transition_model=transition_model)
-    # smoother = IPLSKalmanSmoother(transition_model=transition_model)
-    track_ipls = smoother.smooth(track_iplf)
+    track_ipls = IPLSKalmanSmoother(transition_model=transition_model).smooth(track_iplf)
 
 
     # Plotting the results using Plotterly
