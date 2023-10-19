1c4db763dea40fac92725dbb236e2a48816350b2
alexeynarykov <a.narykov@liverpool.ac.uk>
2023-10-19 17:40:35 +0100

    Updated version.

--- stonesoup/robusstod/physics/other.py
+++ stonesoup/robusstod/physics/other.py
@@ -5,7 +5,7 @@ def get_noise_coefficients(GM):
     We need these values that drive the process noise in the prediction step.
     """
     # ratio = 1e-2  # 2 orders are suggested by Simon following a Lee's manuscript
-    ratio = 1e-14  # otherwise it `lands in Bolivia'
+    ratio = 1e-11  # otherwise it `lands in Bolivia'
     st_dev = ratio * GM
     q = st_dev ** 2
     q_xdot, q_ydot, q_zdot = q, q, q
