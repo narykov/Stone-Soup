540bc8eb9c43fb78b43bf41b57d99c30259ac136
alexeynarykov <a.narykov@liverpool.ac.uk>
2023-12-05 14:08:22 +0000

    Updated slr definition (includes astype(float)) to avoid Bearing messing up the linearisation.

--- stonesoup/robusstod/stonesoup/functions.py
+++ stonesoup/robusstod/stonesoup/functions.py
@@ -14,7 +14,7 @@ def slr_definition(state_pdf, prediction):
     p_matrix = state_pdf.covar
 
     # The predicted quantities wrt the state pdf (e.g. using sigma points)
-    z_bar = prediction.state_vector
+    z_bar = prediction.state_vector.astype(float)
     # next quantity (psi) is naturally available in GaussianMeasurementPrediction for predicted measurements,
     # but has been introduced to AugmentedGaussianStatePrediction for state predictions
     psi = prediction.cross_covar
@@ -24,6 +24,10 @@ def slr_definition(state_pdf, prediction):
     H_plus = psi.T @ np.linalg.inv(p_matrix)
     b_plus = z_bar - H_plus @ x_bar
     Omega_plus = phi - H_plus @ p_matrix @ H_plus.T
+
+    # sanity check
+    if not any(np.isclose(z_bar.astype(float), b_plus + H_plus @ x_bar, rtol=1e-05, atol=1e-08)):
+        print('Issues in the SLR calculations')
 
     # The output is the function's SLR with respect to the state_pdf
     return H_plus, b_plus, Omega_plus
